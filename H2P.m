20682109cc433bd820974ba629cde67ee045a281
ParisRomane <romane.piing@gmail.com>
2023-03-24 18:01:44 +0100

    cube - TP 1 fin

--- H2P.m
+++ H2P.m
@@ -2,7 +2,7 @@ function [P,s]=H2P(H,K)
 Rt = inv(K)*H;
 n = norm(Rt(:,1));
 Rt = Rt/n;
-...
-P = K*[r1 r2 r3 t];
+s = norm(Rt(:,1))/norm(Rt(:,2));
+P = K*[Rt(:,1) Rt(:,2)/s cross(Rt(:,1),Rt(:,2))  Rt(:,3)]
 P = P/P(3,4);
 end
